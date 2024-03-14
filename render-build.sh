3158124523716c33ba2d95a977c6d1ae15b69b74
ephcratSO <118641681+ephcratSO@users.noreply.github.com>
2024-03-14 18:26:18 -0300

    playwright render build test

---
+++ render-build.sh
@@ -0,0 +1,12 @@
+set -e
+
+yarn add @playwright/browser-chromium
+
+# Store/pull Playwright cache with build cache
+if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
+  echo "...Copying Playwright Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+else 
+  echo "...Storing Playwright Cache in Build Cache" 
+  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
+fi
